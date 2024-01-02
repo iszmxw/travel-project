0ab173c6d461d7ae3b81913d1c4ed1a090c22190
Ethan Yep <mail@54zm.com>
2024-01-02 19:22:24 +0800

    Test

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,45 @@
 <template>
-  <van-button type="success" size="large" @click="handleClick">ok</van-button>
-  <van-button type="primary">主要按钮</van-button>
-  <!-- 可以使用 CellGroup 作为容器 -->
-  <van-cell-group inset>
-    <van-field v-model="username" label="文本" placeholder="请输入用户名" />
-  </van-cell-group>
+  <div>
+    <van-tabs v-model:active="active">
+      <van-tab title="现代简约"><div class="cont cont1">现代简约</div></van-tab>
+      <van-tab title="新中式"><div class="cont cont2">新中式</div></van-tab>
+      <van-tab title="轻奢"><div class="cont cont3">轻奢</div></van-tab>
+      <van-tab title="美式"><div class="cont cont4">美式</div></van-tab>
+      <van-tab title="欧式"><div class="cont cont5">欧式</div></van-tab>
+    </van-tabs>
+  </div>
 </template>
 
 <script setup>
-import { ref,reactive} from "vue"
-const handleClick = () => {
-  console.log("我被点击了");
-}
+import { ref } from 'vue';
+const active = ref(0);
 
-const username = ref("");
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+/deep/ .van-tab__text{
+  font-size: 32px;
+  padding: 32px 0 28px 0;
+}
+.cont{
+  width: 620px;
+  height: 360px;
+  border-radius: 8px;
+  margin: 40px 98px 40px 32px;
+}
+.cont1{
+  background: #e4d3d3;
+}
+.cont2{
+  background: #c1c498;
+}
+.cont3{
+  background: #86c296;
+}
+.cont4{
+  background: #7b7fc5;
+}
+.cont5{
+  background: #bc75b1;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,18 +1,15 @@
 import { createApp } from "vue";
 // import './style.css'
 import App from "./App.vue";
-
-// 1. 引入你需要的组件
-import { Button, Field, CellGroup } from "vant";
 // 2. 引入组件样式
 import "vant/lib/index.css";
-// 引入 rem 相关依赖
-import "amfe-flexible";
+
+// 1. 引入你需要的组件
+import { Tab, Tabs } from 'vant';
 
 const app = createApp(App);
 // 3. 注册你需要的组件
-app.use(Button);
-app.use(Field);
-app.use(CellGroup);
+app.use(Tab);
+app.use(Tabs);
 
 app.mount("#app");
